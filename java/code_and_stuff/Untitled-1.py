464c8b648214404e06790f9b1a7a394003de702e
not-lucky <57046712+not-lucky@users.noreply.github.com>
2023-05-31 05:58:32 +0530

    lazy stuff

--- java/code_and_stuff/Untitled-1.py
+++ java/code_and_stuff/Untitled-1.py
@@ -1,15 +1,16 @@
-from random import choice
+from collections import Counter
 
-a = ['MCA', 'BCA', 'M.SC IT']
-month = ['JAN', 'FEB', 'MAR', 'OCT', 'NOV']
+input()
+a = Counter(map(int, input().split()))
 
-for i in range(6):
-  print(f"INSERT INTO STUDENT VALUES ({i}, 'S_{i}', '{i+1}-{choice(month)}-{i}', {i});")
-  
-for i in range(100, 105):
-  print(f"INSERT INTO FACULTY VALUES ({i}, 'F_{i}', '{choice(a)}', {i*100});")
-  
-for i in range(200, 205):
-  print(f"INSERT INTO COURSE VALUES ({i}, 'C_{i}', {i+100}, '{choice(a)}');")
- 
- 
+input()
+b = Counter(map(int, input().split()))
+
+out = []
+
+for k, v in b.items():
+    ans = a.get(k, 0)
+    out += [k for i in range(v-ans)]
+        
+out.sort()
+print(out)
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,48 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int part(int a[], int s, int e) {
-  int p = a[e];
-  int i = s - 1;
+// Structure for the graph node
+struct Node {
+    int vertex;
+    struct Node* next;
+};
 
-  for (int j = s; j < e; j++) {
-    if (a[j] < p) {
-      i++;
+// Function to create a new graph node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
 
-      int t = a[i];
-      a[i] = a[j];
-      a[j] = t;
+// Function to add an edge to the graph
+void addEdge(struct Node** graph, int src, int dest) {
+    // Create a new node for the destination vertex
+    struct Node* newNode = createNode(dest);
+
+    // Add the new node to the adjacency list of the source vertex
+    newNode->next = graph[src];
+    graph[src] = newNode;
+
+    // Since it is an undirected graph, add an edge from dest to src as well
+    newNode = createNode(src);
+    newNode->next = graph[dest];
+    graph[dest] = newNode;
+}
+
+// Function to print the graph
+void printGraph(struct Node** graph, int vertices) {
+    for (int i = 0; i < vertices; ++i) {
+        struct Node* currentNode = graph[i];
+        printf("Adjacency list of vertex %d: ", i);
+        while (currentNode) {
+            printf("%d -> ", currentNode->vertex);
+            currentNode = currentNode->next;
+        }
+        printf("NULL\n");
     }
-  }
-
-  int t = a[e];
-  a[e] = a[i+1];
-  a[i+1] = t;
-
-  return i+1;
-}
-
-
-void quick(int a[], int s, int e) {
-  if (s < e) {
-    int p = part(a, s, e);
-    quick(a, s, p - 1);
-    quick(a, p+1, e);
-  }
-}
-
-void print(int a[], int n) {
-  for (int i =0;i<n;i++) {
-    printf("%d, ", a[i]);
-  }
-  printf("\n\n");
 }
 
 int main() {
-  int a[] = {1,5,6,3,8,8,6,3,9,2};
-  print(a, 10);
+    int vertices = 5;
+    struct Node** graph = (struct Node**)malloc(vertices * sizeof(struct Node*));
 
-  quick(a, 0, 9);
+    // Initialize the graph with NULL values
+    for (int i = 0; i < vertices; ++i) {
+        graph[i] = NULL;
+    }
 
-  print(a, 10);
+    // Add edges to the graph
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 4);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 1, 4);
+    addEdge(graph, 2, 3);
+    addEdge(graph, 3, 4);
 
+    // Print the graph
+    printGraph(graph, vertices);
+
+    // Free the memory allocated for the graph
+    for (int i = 0; i < vertices; ++i) {
+        struct Node* currentNode = graph[i];
+        while (currentNode) {
+            struct Node* temp = currentNode;
+            currentNode = currentNode->next;
+            free(temp);
+        }
+    }
+    free(graph);
+
+    return 0;
 }
